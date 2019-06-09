b8630215081b00bb2935c8ee616f9cb689330eaa
Naomi Kaduwela <43353497+yayitsnaomi@users.noreply.github.com>
2019-06-09 15:01:01 -0500

    adding config for flask app - RDS & S3 connections

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+DEBUG = True
+LOGGING_CONFIG = "config/logging/local.conf"
+PORT = 9020
+APP_NAME = "AI_healthcare"
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+conn_type = "mysql+pymysql"
+user = os.environ.get("MYSQL_USER")
+password = os.environ.get("MYSQL_PASSWORD")
+host = os.environ.get("MYSQL_HOST")
+port = os.environ.get("MYSQL_PORT")
+DATABASE_NAME = 'msia423'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+HOST = "127.0.0.1"
+MAX_ROWS_SHOW = 15
+S3BUCKET = 'healthcarechatbot'
+S3_MODEL_PATH: 's3://healthcarechatbot/data/model.pkl'
