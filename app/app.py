0c49145f33980bbcfc76621f0939feb49367d64b
Naomi Kaduwela <43353497+yayitsnaomi@users.noreply.github.com>
2019-06-10 01:07:10 -0500

    updating app.py with try catch

--- app/app.py
+++ app/app.py
@@ -50,60 +50,60 @@ def add_entry():
     Returns: rendered html template with evaluation results.
     """
     # retrieve features
-    #try:
-    logger.info("start retrieving features from UI!")
+    try:
+        logger.info("start retrieving features from UI!")
 
-    age = request.form['age']
-    sex = request.form['sex']
-    cp = request.form['cp']
-    trestbps = request.form['trestbps']
-    chol = request.form['chol']
-    fbs = request.form['fbs']
-    restecg = request.form['restecg']
-    thalach = request.form['thalach']
-    exang = request.form['exang']
-    oldpeak = request.form['oldpeak']
-    slope = request.form['slope']
-    ca = request.form['ca']
-    thal = request.form['thal']
-    logger.info("all inputs retrieved!")
+        age = request.form['age']
+        sex = request.form['sex']
+        cp = request.form['cp']
+        trestbps = request.form['trestbps']
+        chol = request.form['chol']
+        fbs = request.form['fbs']
+        restecg = request.form['restecg']
+        thalach = request.form['thalach']
+        exang = request.form['exang']
+        oldpeak = request.form['oldpeak']
+        slope = request.form['slope']
+        ca = request.form['ca']
+        thal = request.form['thal']
+        logger.info("all inputs retrieved!")
 
-    # load trained model form s3
-    logger.info("loading model from s3!")
+        # load trained model form s3
+        logger.info("loading model from s3!")
 
-    session = boto3.session.Session(region_name='eu-west-1')
-    s3client = session.client('s3')
-    response = s3client.get_object(Bucket=app.config["S3BUCKET"], Key="model.pkl")
-    body = response['Body'].read()
-    model = pickle.loads(body)
+        session = boto3.session.Session(region_name='eu-west-1')
+        s3client = session.client('s3')
+        response = s3client.get_object(Bucket=app.config["S3BUCKET"], Key="model.pkl")
+        body = response['Body'].read()
+        model = pickle.loads(body)
 
-    # predicting heart disease with user input
-    predicted = model.predict(pd.DataFrame({'age': age,
-                                            'sex': sex,
-                                            'cp': cp,
-                                            'trestbps': trestbps,
-                                            'chol': chol,
-                                            'fbs': fbs,
-                                            'restecg': restecg,
-                                            'thalach': thalach,
-                                            'exang': exang,
-                                            'oldpeak': oldpeak,
-                                            'slope': slope,
-                                            'ca': ca,
-                                            'thal': thal
-                                            }, index=[0]))
+        # predicting heart disease with user input
+        predicted = model.predict(pd.DataFrame({'age': age,
+                                                'sex': sex,
+                                                'cp': cp,
+                                                'trestbps': trestbps,
+                                                'chol': chol,
+                                                'fbs': fbs,
+                                                'restecg': restecg,
+                                                'thalach': thalach,
+                                                'exang': exang,
+                                                'oldpeak': oldpeak,
+                                                'slope': slope,
+                                                'ca': ca,
+                                                'thal': thal
+                                                }, index=[0]))
 
-    # translate output into readable prediction for UI
-    if(predicted ==0):
-        result = "Patient is not at risk for heart disease"
-    else:
-        result = "Patient is at risk of having heart disease"
+        # translate output into readable prediction for UI
+        if(predicted ==0):
+            result = "Patient is not at risk for heart disease"
+        else:
+            result = "Patient is at risk of having heart disease"
 
-    # render input page with updated result
-    return render_template('index_heart.html', result=result)
-    #except:
-       # logger.warning("Not able to display homepage, error page returned")
-        #return render_template('error.html') # error if index not found
+        # render input page with updated result
+        return render_template('index_heart.html', result=result)
+    except:
+        logger.warning("Not able to display homepage, error page returned")
+        return render_template('error.html') # error if index not found
 
 if __name__ == "__main__":
     app.run(debug=app.config["DEBUG"], port=app.config["PORT"], host=app.config["HOST"])
